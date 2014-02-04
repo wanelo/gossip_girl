6ace8afbea1042f82a159c4c1667d77bf4b65308
Maciej Pieszała <maciej.pieszala@allegro.pl>
2014-02-04 16:45:52 +0100

    send multiple stats in one udp package

--- backends/gossip_girl.js
+++ backends/gossip_girl.js
@@ -5,14 +5,15 @@ function GossipGirl(startupTime, config, emitter) {
   var self = this
   self.config = config.gossip_girl || []
   self.statsd_config = config
-  self.ignorable = [ "statsd.packets_received", "statsd.bad_lines_seen", "statsd.packet_process_time" ]
+  self.ignorable = [ "statsd.packets_received", "statsd.bad_lines_seen", "statsd.packet_process_time", "statsd.timestamp_lag" ]
 
   emitter.on('flush', function(time_stamp, metrics) { self.process(time_stamp, metrics); })
 }
 
 GossipGirl.prototype.gossip = function(packet, host, port) {
   var self = this
-  self.sock.send(packet, 0, packet.length, port, host, function(err,bytes) {
+  var buffer = new Buffer(packet)
+  self.sock.send(buffer, 0, buffer.length, port, host, function(err,bytes) {
     if (err) {
       console.log(err)
     }
@@ -20,18 +21,18 @@ GossipGirl.prototype.gossip = function(packet, host, port) {
 }
 
 GossipGirl.prototype.format = function (key, value, suffix) {
-  return new Buffer("'" + key + "':" + value + "|" + suffix)
+  return "'" + key + "':" + value + "|" + suffix + "\n"
 }
 
 GossipGirl.prototype.process = function(time_stamp, metrics) {
   var self = this
   hosts = self.config
-  var stats, packet
+  var stats, packetTmp, packet = "", countStats = 0, countPacket = 0, countValues = 0
 
   var stats_map = {
-    counters: { data: metrics.counters, suffix: "c",  name: "counter" },
-    gauges:   { data: metrics.gauges,   suffix: "g",  name: "gauge" },
-    timers:   { data: metrics.timers,   suffix: "ms", name: "timer" }
+    counters: { data: metrics.counters, suffix: "c", name: "counter" },
+    gauges: { data: metrics.gauges, suffix: "g", name: "gauge" },
+    timers: { data: metrics.timers, suffix: "ms", name: "timer" }
   }
 
   self.sock = dgram.createSocket("udp4")
@@ -40,14 +41,42 @@ GossipGirl.prototype.process = function(time_stamp, metrics) {
       stats = stats_map[type]
       for (key in stats.data) {
         if (self.ignorable.indexOf(key) >= 0) continue
-        packet = self.format(key, stats.data[key], stats.suffix)
+
+        packetTmp = self.format(key, stats.data[key], stats.suffix)
 
         if (self.statsd_config.dumpMessages) {
           util.log ("Gossiping about " + stats.name + ": " + packet)
         }
-
-        self.gossip(packet, hosts[i].host, hosts[i].port)
-      } 
+        countStats++
+        countValues += stats.data[key]
+        if (!self.config[i].packageJoin) {
+          self.gossip(packetTmp, hosts[i].host, hosts[i].port);
+          countPacket++
+        } else {
+          if (packet.length + packetTmp.length > self.config[i].packageLimit) {
+            setTimeout(function(packet, hosts, i){
+              self.gossip(packet, hosts[i].host, hosts[i].port)
+            }, self.config[i].delay * countPacket, packet, hosts, i);
+            packet = packetTmp;
+            countPacket++
+	  } else {
+            packet = packet + packetTmp;
+          }
+        }
+      }
+      if (packet.length > 0) {
+        setTimeout(function(packet, hosts, i){
+          self.gossip(packet, hosts[i].host, hosts[i].port)
+        }, self.config[i].delay * countPacket, packet, hosts, i);
+        countPacket++
+      }
+      if (self.config[i].verbose) {
+        util.log ("Gossiping " + type + " : " + countStats + "(" + countValues + ") stats in " + countPacket + " packages")
+      }
+      countPacket = 0
+      countStats = 0
+      countValues = 0
+      packet = ""
     }
   }
 }
